5e02fcd40139819175d06528741f9fbffb3d5beb
Shukurullo <shukurulloshokiraliyev699@gmail.com>
2025-11-15 17:00:35 +0500

    deploy eror

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../pages/Header/header';
+import Header from '../pages/Header';
 import Footer from '../pages/Footer/index';
 
 const Layout = () => {
